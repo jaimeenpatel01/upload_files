2ed755a89aa869a374dd91d61cd14e6f3d8c368b
Jaimeenpatel01 <jaimeenpatel2017@gmail.com>
2023-12-28 14:26:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const PORT = 8001;
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+const upload = multer({ storage });
+
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    return res.render("homepage");
+});
+
+app.post("/upload", upload.single("profileImage"), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+
+    return res.redirect("/");
+})
+
+app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
